7926ace3f9042e933c836d1d6552588b846c16a1
rjazmirkhan <bangladeshfreelancerazmir@gmail.com>
2025-01-18 02:15:35 +0600

    cartsClearall setup

--- index.js
+++ index.js
@@ -246,10 +246,17 @@ async function run() {
     })
 
 
-    app.delete("/cartd/:id", async (req, res) => {
+    app.delete("/carts/:id", async (req, res) => {
       const id = req.params.id
       const query = { _id: new ObjectId(id) };
       const result = await cartsCollection.deleteOne(query);
+      res.send(result)
+    })
+
+    app.delete("/cartsClear", async (req, res) => {
+      const id = req.params.id
+      const query = {};
+      const result = await cartsCollection.deleteMany(query);
       res.send(result)
     })
 
